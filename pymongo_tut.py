41bbce0315631d22b8698db65b04b8f2901b1aca
jiminsong490 <misosjm@naver.com>
2023-03-21 00:49:28 +0900

    feat : mysql에 업로드 구현

--- pymongo_tut.py
+++ pymongo_tut.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from pymysql import connect
 from datetime import datetime
 from dotenv import load_dotenv
 import pandas as pd
@@ -29,7 +30,50 @@ def insert_mongo(): #파일을 읽어 mongodb에 데이터 추가
     collection.insert_many(file.to_dict('records'))
     print('mongodb에 업로드')
 
+def insert_mysql():
+        # mysql 연결
+        try:
+            mysql_con = connect(
+                host="127.0.0.1",
+                user="root",
+                password="0000",
+                database="checkit"
+            )
+            print('mysql 연결 완료')
+        except Exception:
+            print("Mysql Error" + Exception)
+        
+        # today = datetime.now().strftime('%Y-%m-%d')
+        mongo_db = mongo_client['check_it']
+        collection = mongo_db['2023-03-11']
+
+        fruits = collection.find()
+        print('mongodb에서 값 가져오기')    
+        mysql_cursor = mysql_con.cursor()
+
+        for data in fruits:
+            title = data['title']
+            author = data['author']
+            publisher = data['publisher']
+            cover_image_url = data['img_url']
+            pages = data['pages']
+            width = data['width']
+            thickness = data['thickness']
+            category = data['category']
+
+            sql = "UPDATE fruit SET price = %s WHERE name = %s"
+            sql = "INSERT INTO books (title, author, publisher, cover_image_url, pages, width, thickness, category) values(%s, %s, %s, %s, %s, %s, %s, %s)" 
+            injection = (str(title),str(author),str(publisher),str(cover_image_url),str(pages),str(width),str(thickness),str(category))
+            mysql_cursor.execute(sql,injection)
+        print('책 내용 입력')
+        mysql_con.commit()
+        print('mysql에 저장')
+        
+        mysql_cursor.close()
+        mysql_con.close()
+
 
 print('데이터 업로드를 시작합니다.......')
-insert_mongo()
+# insert_mongo()
+insert_mysql()
 print('데이터 업로드 종료......')
